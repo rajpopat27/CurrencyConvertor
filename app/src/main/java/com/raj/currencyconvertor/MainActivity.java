ba5b010da6b0f54e1008f2476f366bf1fe22f1f8
Raj Popat <rajpopat27@gmail.com>
2018-09-06 20:22:47 +0530

    Initial commit

---
+++ app/src/main/java/com/raj/currencyconvertor/MainActivity.java
@@ -0,0 +1,55 @@
+package com.raj.currencyconvertor;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void currencyConvert(View view){
+        EditText usd = (EditText)findViewById(R.id.usd);
+        Log.i("Dollar",usd.getText().toString());
+        Double dollar = Double.parseDouble(usd.getText().toString());
+        Double inr = 64.43*dollar;
+        Log.i("Indian ruppes",inr.toString());
+        Toast.makeText(getApplicationContext(),"Rs = "+inr.toString(),Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
